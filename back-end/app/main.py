c706cb72b89a879416a0a8f74905eac716d37e47
ahmedz-5645 <ahmed.zafar5645@gmail.com>
2026-03-19 16:19:49 -0400

    ofc theres cors issues.

--- back-end/app/main.py
+++ back-end/app/main.py
@@ -4,10 +4,19 @@ from sqlmodel import SQLModel
 from app.database import engine
 from app.routers import schedule, history, psw
 
+from fastapi.middleware.cors import CORSMiddleware
+
 # Create the tables immediately when Python reads this file
 SQLModel.metadata.create_all(engine)
 
 app = FastAPI(title="PSW Scheduling API")
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"], # In production, you'd be more specific!
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 # Plug in the routers
 app.include_router(schedule.router)
